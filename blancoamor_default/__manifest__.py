2a78330376535a1d2fff4d058210868b0ceda089
filoquin <filoquin@gmail.com>
2021-12-16 09:31:12 -0300

    [FIX] Quito el basedir

--- blancoamor_default/__manifest__.py
+++ blancoamor_default/__manifest__.py
@@ -25,18 +25,18 @@
     'odoo-license': 'CE',
 
     # port where odoo starts serving pages
-    'port': '8069',
+    'port': '8016',
 
     # list of url repos to install in the form 'repo-url directory'
     'git-repos': [
         'https://github.com/filoquin/cl-blancoamor.git',
         'git@code.gestionblancoamor.com:odoo-13/blancoamor.git'
     ],
-    'base_dir': '/opt/odoo/docker_files/',
     'docker-images': [
         'odoo regaby/odoo-ce:13.0',
         # 'postgres postgres:10.1-alpine',
         #'postgres mdillon/postgis:11-alpine',
         #'nginx nginx'
     ]
+    #'base_dir': '/opt/odoo/docker_files/'
 }
